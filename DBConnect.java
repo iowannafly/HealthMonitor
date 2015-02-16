bb3d586fe52fdb4d402f8c1eb7b5e49a5716701b
Guleida <guleid.ali@my.westminster.ac.uk>
2015-02-16 14:43:50 +0000

    Update DBConnect.java

--- DBConnect.java
+++ DBConnect.java
@@ -1,4 +1,4 @@
-mport java.sql.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnect {
@@ -7,19 +7,15 @@ public class DBConnect {
 
         public static Connection connectDB() {
 	        try {
-		            Class.forName("com.mysql.jdbc.Driver");
-			                String dbhost = "jdbc:mysql://localhost:2222/w1427818_0";
-					            String dbuser = "w1427818";
-						                String dbpass = "2eV9cWzsDvZ5";
-								            Connection con = DriverManager.getConnection(dbhost, dbuser, dbpass);
-									                //System.out.println("Connected ");
-											            return con;
-												            } catch (Exception err) {
-													                System.out.println(err.getMessage());
-															            return null;
-																            }
-
-																	        }
-
-																		}
-										/*Java file connection to the databse*/
+		 Class.forName("com.mysql.jdbc.Driver");
+	         String dbhost = "jdbc:mysql://localhost:2222/w1427818_0";
+	         String dbuser = "w1427818";
+		 String dbpass = "2eV9cWzsDvZ5";
+		 Connection con = DriverManager.getConnection(dbhost, dbuser, dbpass);
+		 //System.out.println("Connected ");
+		 return con;
+		 } catch (Exception err) {
+		System.out.println(err.getMessage());
+		}
+	
+	/*Java file connection to the databse*/
